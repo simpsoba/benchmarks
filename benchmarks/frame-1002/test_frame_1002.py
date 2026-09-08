d060d3d910ca2026d991609ebf5049e79468cae3
Claudio Perez <50180406+claudioperez@users.noreply.github.com>
2026-09-08 16:33:09 -0700

    Create test_frame_1002.py

---
+++ benchmarks/frame-1002/test_frame_1002.py
@@ -0,0 +1,13 @@
+#
+# Cantilever with transverse loading, finite deformation
+#
+# [1] C. M. Perez and F. C. Filippou, 
+#     “On nonlinear geometric transformations of finite elements,” 
+#     Numerical Meth Engineering, vol. 125, no. 17, Sep. 2024, doi: 10.1002/nme.7506.
+#
+from xara.examples.perez2024nonlinear import Ex_4_2_2
+
+
+def test_Ex_4_2_2():
+    Ex_4_2_2()
+
